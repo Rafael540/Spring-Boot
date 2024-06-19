f80d15abfba67f663025eb93805c4ab1792acbd7
Rafael540 <alves540@hotmail.com>
2024-06-19 17:08:49 -0300

    Commit Spring Boot

---
+++ App-Dio/App-Dio/src/main/java/dio/App_Dio/AppDioApplication.java
@@ -0,0 +1,13 @@
+package dio.App_Dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDioApplication.class, args);
+	}
+
+}
---
+++ App-Dio/App-Dio/src/test/java/dio/App_Dio/AppDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.App_Dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
